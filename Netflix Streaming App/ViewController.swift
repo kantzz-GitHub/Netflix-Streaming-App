3c1115bbb11c52b2fe42d60dd3c546d201cd6270
kantzz <codingblyatt@gmail.com>
2022-05-12 19:24:34 +0500

    Initial Commit

---
+++ Netflix Streaming App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix Streaming App
+//
+//  Created by admin on 2022-05-12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
