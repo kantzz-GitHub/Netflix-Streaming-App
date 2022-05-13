4e8e9d8c61f076580eb9c66abf68238ccf5e7590
kantzz <codingblyatt@gmail.com>
2022-05-13 21:26:21 +0500

    Implementing Play button in Upcoming ViewController

---
+++ Netflix Streaming App/ViewModels/TitleViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  TitleViewModel.swift
+//  Netflix Streaming App
+//
+//  Created by admin on 2022-05-13.
+//
+
+import Foundation
+
+struct TitleViewModel{
+    let titleName: String
+    let posterURL: String
+    
+}
