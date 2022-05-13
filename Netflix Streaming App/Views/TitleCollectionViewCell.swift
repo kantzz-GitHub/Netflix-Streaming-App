60d6ee45ee558048bc0d430fd95b1dcaf121b88a
kantzz <codingblyatt@gmail.com>
2022-05-13 20:06:47 +0500

    Making title images display in the application

--- Netflix Streaming App/Views/TitleCollectionViewCell.swift
+++ Netflix Streaming App/Views/TitleCollectionViewCell.swift
@@ -35,7 +35,11 @@ class TitleCollectionViewCell: UICollectionViewCell {
     
     public func configure(with model: String){
     
-        print(model)
+        guard let url = URL(string: "https://image.tmdb.org/t/p/w500/\(model)") else {
+            return
+        }
+        
+        posterImageView.sd_setImage(with: url, completed: nil)
     }
     
 }
