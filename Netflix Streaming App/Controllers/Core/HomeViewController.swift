5cf0fd9cdbd423b6aab0f9aa482c8f13b382c8d7
kantzz <codingblyatt@gmail.com>
2022-05-14 13:59:58 +0500

    Making the Hero Title change every time the application is opened

--- Netflix Streaming App/Controllers/Core/HomeViewController.swift
+++ Netflix Streaming App/Controllers/Core/HomeViewController.swift
@@ -17,6 +17,10 @@ enum Sections: Int {
 
 class HomeViewController: UIViewController {
     
+    
+    private var randomTrendingMovie: Title?
+    private var headerView:HeroHeaderUIView?
+    
     let sectionTitles: [String] = ["Trending Movies", "Trending TV",  "Popular", "Upcoming Movies", "Top Rated"]
     
     private let homeFeedTable: UITableView = {
@@ -37,11 +41,27 @@ class HomeViewController: UIViewController {
         
         configureNavBar()
         
-        let headerView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
+        
+        
+        headerView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
         homeFeedTable.tableHeaderView = headerView
+        configureHeroHeaderView()
+    }
+    
+    private func configureHeroHeaderView(){
         
-
-        
+        APICaller.shared.getTrendingMovies { [weak self] result in
+            switch result {
+            case .success(let titles):
+                
+                let selectedTitle = titles.randomElement()
+                self?.randomTrendingMovie = selectedTitle
+                
+                self?.headerView?.configure(with: TitleViewModel(titleName: selectedTitle?.original_title ?? "", posterURL: selectedTitle?.poster_path ?? ""))
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
         
     }
     
